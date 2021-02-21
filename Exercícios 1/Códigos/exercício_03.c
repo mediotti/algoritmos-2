e06b6f46f7f5a5670cefcbb5f4fe00e9fd624191
medzdev <medzdeveloper@gmail.com>
2021-02-20 21:19:22 -0300

    acrescimo da biblioteca stdio.h nos codigos

---
+++ Exercícios 1/Códigos/exercício_03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int num;
+        printf("\nInsira um numero inteiro para descobrir se ele e par ou impar: ");
+        scanf("%d", &num);
+            funcParImpar(num);
+    return 0;
+}
