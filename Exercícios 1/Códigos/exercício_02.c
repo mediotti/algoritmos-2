276ed36cbb9774b0d9079146f8eb69243f323adb
medzdev <medzdeveloper@gmail.com>
2021-02-20 21:14:43 -0300

    Resolução do segundo exercicio da lista

---
+++ Exercícios 1/Códigos/exercício_02.c
@@ -0,0 +1,15 @@
+void funcMaiorMenor(float num1, float num2){
+    if (num1>num2){
+        printf("\nO numero %.2f e maior que %.2f", num1, num2);  
+    }
+    else if(num2>num1){
+        printf("\nO numero %.2f e maior que %.2f", num2, num1);
+    }
+}
+int main(int argc, char **argv){
+    float num1, num2;
+        printf("\nInsira dois numeros e descubra qual deles e maior: ");
+        scanf("%f %f", &num1, &num2);
+            funcMaiorMenor(num1, num2);
+    return 0;
+}
